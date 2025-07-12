9eab429c6440aec77ffe43f34754ea771e7dc8fc
Carlos Echeverría Moscoso <116301628+ElegantCharles@users.noreply.github.com>
2025-07-12 17:16:49 -0400

    Create tailwind-config.js

---
+++ tailwind-config.js
@@ -0,0 +1,16 @@
+/* Extensión de paleta corporativa y tipografía */
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "law-pink-light":  "#ffeef5",
+        "law-pink-medium": "#ffccd9",
+        "law-pink-dark":   "#ff9ab3",
+        "law-gray-passed": "#d1d5db"      // gray-300
+      },
+      fontFamily: {
+        pop: ["Poppins", "sans-serif"]
+      }
+    }
+  }
+};
